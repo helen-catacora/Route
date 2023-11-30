4668cc2f67985540c7a51aba8b6e60047c8bcdc6
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-11-30 12:09:08 -0400

    olda

---
+++ src/componentes/Span.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Span() {
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Dashboard</li>
+            <li>Header</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Span
